32c5033b8002528a7c418aeed45b7ca8b13d46de
Patrick Coxall <mr.coxall@mths.ca>
2023-09-13 13:13:31 -0700

    typescript version

--- main.ts
+++ main.ts
@@ -2,7 +2,22 @@
  *
  * Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program ...
+ * This program turns an LED on a breadboard on and off.
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.clearScreen()
+pins.digitalWritePin(DigitalPin.P16, 0)
+basic.showIcon(IconNames.Happy)
+
+// turn on LED
+input.onButtonPressed(Button.A, function () {
+  basic.showIcon(IconNames.Yes)
+  pins.digitalWritePin(DigitalPin.P16, 1)
+})
+
+// turn off LED
+input.onButtonPressed(Button.B, function () {
+  basic.showIcon(IconNames.No)
+  pins.digitalWritePin(DigitalPin.P16, 0)
+})
